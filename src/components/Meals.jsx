0465a8e0257362983415ff2362f6fbe20d8d2441
anouar garbaa <g.anouar@yahoo.com>
2024-10-20 12:13:51 +0100

    adding custom useHttp hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,30 +1,61 @@
-
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+ const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, [] );
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  if (isLoading) {
+    return <p>Fetching meals...</p>
+  }
 
-      if (!response.ok) {
-        // ...
-      }
-
-      const meals = await response.json(); // Convert response to JS  objet
-      setLoadedMeals(meals);
-    }
-
-    fetchMeals();
-  }, []);
+  // if (!data) {
+  //   return <p>No meals found.</p>
+  // }
 
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-       <MealItem key={meal.id} meal={meal} />
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
 }
+
+
+
+//& without custom useHttp.js hook
+// import { useEffect, useState } from "react";
+// import MealItem from "./MealItem";
+
+// export default function Meals() {
+//   const [loadedMeals, setLoadedMeals] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchMeals() {
+//       const response = await fetch("http://localhost:3000/meals");
+
+//       if (!response.ok) {
+//         // ...
+//       }
+
+//       const meals = await response.json(); // Convert response to JS  objet
+//       setLoadedMeals(meals);
+//     }
+
+//     fetchMeals();
+//   }, []);
+
+//   return (
+//     <ul id="meals">
+//       {loadedMeals.map((meal) => (
+//        <MealItem key={meal.id} meal={meal} />
+//       ))}
+//     </ul>
+//   );
+// }
