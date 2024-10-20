01ffbdbf8c09492c6bb5629b95cc55f2d82f31a1
anouar garbaa <g.anouar@yahoo.com>
2024-10-20 12:34:13 +0100

    handling the error state

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 import MealItem from "./MealItem";
 
 const requestConfig = {};
@@ -11,8 +12,16 @@ export default function Meals() {
   } = useHttp("http://localhost:3000/meals", requestConfig, [] );
 
   if (isLoading) {
-    return <p>Fetching meals...</p>
+    return <p className="center">Fetching meals...</p>
   }
+
+if(error) {
+  return <Error title="Failed to fetch meals" message={error} />
+}
+
+
+
+
 
   // if (!data) {
   //   return <p>No meals found.</p>
